9840269692279fb936c1b39a2663196089b72056
calvin-vin <calvinsan123@gmail.com>
2025-01-23 07:37:10 +0700

    deploy to ec2

--- mongoC.js
+++ mongoC.js
@@ -1,13 +1,14 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+// const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const connectionString = `mongodb+srv://calvinsan123:${password}@devcluster.b2s0y.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`; // clustore url
 const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
-  console.log("connection successful")
-} catch(e) {
+  console.log("connection successful");
+} catch (e) {
   console.error(e);
 }
 let db = conn.db("integration_ninjas");
